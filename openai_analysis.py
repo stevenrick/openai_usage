71c665f526c88b575f0436e5a42f2d03dc725aa9
stevenrick <srick@mit.edu>
2023-07-18 11:27:38 -0400

    cleans up and functionalizes code

--- openai_analysis.py
+++ openai_analysis.py
@@ -2,76 +2,83 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 from matplotlib import pyplot as plt
-
 from openai_model_costs import model_map_cost_per_1k
 
+# # # setup # # #
 load_dotenv()
+data_dir = os.getenv('data_dir')
+finetune_models = os.getenv('finetune_models')
+usage_path = os.path.join(data_dir, "openai_usage.csv")
 
 
 def add_cost_cols(_df):
-    model_list = _df["model"].tolist()
-    context_cost_per_1k = []
-    generated_cost_per_1k = []
-    for m in model_list:
+    _model_list = _df["model"].tolist()
+    _context_cost_per_1k = []
+    _generated_cost_per_1k = []
+    for _m in _model_list:
         try:
-            if m in finetune_models:
-                m = "finetune"
-            context_cost_per_1k.append(model_map_cost_per_1k[m][0])
-            generated_cost_per_1k.append(model_map_cost_per_1k[m][1])
+            if _m in finetune_models:
+                _m = "finetune"
+            _context_cost_per_1k.append(model_map_cost_per_1k[_m][0])
+            _generated_cost_per_1k.append(model_map_cost_per_1k[_m][1])
         except KeyError:
-            print(m, 'not in model map')
-    _df["context_cost_per_1k"] = context_cost_per_1k
-    _df["generated_cost_per_1k"] = generated_cost_per_1k
+            print(_m, 'not in model map')
+    _df["context_cost_per_1k"] = _context_cost_per_1k
+    _df["generated_cost_per_1k"] = _generated_cost_per_1k
     return _df
 
 
-data_dir = os.getenv('data_dir')
-finetune_models = os.getenv('finetune_models')
-usage_path = os.path.join(data_dir, "open_ai_usage.csv")
+def add_costs_and_sum_by_day():
+    _df = pd.read_csv(usage_path)
+    _df = add_cost_cols(_df)
+    _df["dollars"] = (_df["context_tokens"] / 1000) * _df["context_cost_per_1k"] +\
+                     (_df["generated_tokens"] / 1000) * _df["generated_cost_per_1k"]
 
-df = pd.read_csv(usage_path)
-df = add_cost_cols(df)
-df["dollars"] = (df["context_tokens"] / 1000) * df["context_cost_per_1k"] +\
-                (df["generated_tokens"] / 1000) * df["generated_cost_per_1k"]
+    _users = _df["username"].unique()
 
-users = df["username"].unique()
+    _sum_dict = {"date": [], "user": [], "context_tokens": [], "generated_tokens": [], "dollars": []}
 
-cta = df["context_tokens"].sum()
-gta = df["generated_tokens"].sum()
-total = cta + gta
+    for _user in _users:
+        _subset = _df[_df["username"] == _user]
+        _groups = _subset.groupby("date")
+        for _group in _groups:
+            # print(_group[0])
+            _ct = _group[1]["context_tokens"].sum()
+            _gt = _group[1]["generated_tokens"].sum()
+            _d = _group[1]["dollars"].sum()
+            # print(_user, _ct, _gt, _d)
+            _sum_dict["date"].append(_group[0])
+            _sum_dict["user"].append(_user)
+            _sum_dict["context_tokens"].append(_ct)
+            _sum_dict["generated_tokens"].append(_gt)
+            _sum_dict["dollars"].append(_d)
 
-sum_dict = {"date": [], "user": [], "context_tokens": [], "generated_tokens": [], "dollars": []}
+    _sum_df = pd.DataFrame(_sum_dict)
+    return _sum_df
 
-for u in users:
-    subset = df[df["username"] == u]
-    groups = subset.groupby("date")
-    for group in groups:
-        # print(group[0])
-        ct = group[1]["context_tokens"].sum()
-        gt = group[1]["generated_tokens"].sum()
-        d = group[1]["dollars"].sum()
-        u_tot = ct + gt
-        # print(u, u_tot, d)
-        sum_dict["date"].append(group[0])
-        sum_dict["user"].append(u)
-        sum_dict["context_tokens"].append(ct)
-        sum_dict["generated_tokens"].append(gt)
-        sum_dict["dollars"].append(d)
 
-sum_df = pd.DataFrame(sum_dict)
+def clean_and_plot_user_usage(_sum_df):
+    _date_list = sorted(_sum_df.date.unique())
+    _dollar_dict = dict()
+    for _d in _date_list:
+        for _user in _sum_df["user"].unique():
+            if _user not in _dollar_dict:
+                _dollar_dict[_user] = []
+            subset = _sum_df[(_sum_df["date"] == _d) & (_sum_df["user"] == _user)]
+            if subset.size != 0:
+                _dollar_dict[_user].append(subset["dollars"].values[0])
+            else:
+                _dollar_dict[_user].append(0)
 
-date_list = sorted(sum_df.date.unique())
-dollar_dict = dict()
-for d in date_list:
-    for user in sum_df["user"].unique():
-        if user not in dollar_dict:
-            dollar_dict[user] = []
-        subset = sum_df[(sum_df["date"] == d) & (sum_df["user"] == user)]
-        if subset.size != 0:
-            dollar_dict[user].append(subset["dollars"].values[0])
-        else:
-            dollar_dict[user].append(0)
+    dollar_plotdf = pd.DataFrame(data=_dollar_dict, index=_date_list)
+    dollar_plotdf.plot.line(subplots=True)
+    plt.show()
 
-dollar_plotdf = pd.DataFrame(data=dollar_dict, index=date_list)
-dollar_plotdf.plot.line(subplots=True)
-plt.show()
+
+def main():
+    df = add_costs_and_sum_by_day()
+    clean_and_plot_user_usage(df)
+
+
+if __name__ == "__main__":
+    main()
--- openai_usage.py
+++ openai_usage.py
@@ -6,83 +6,106 @@ import datetime
 from requests.adapters import HTTPAdapter, Retry
 from dotenv import load_dotenv
 
-load_dotenv()
-
+# # # setup # # #
 s = requests.Session()
-
 retries = Retry(total=20,
                 backoff_factor=1,
                 status_forcelist=[429, 500, 502, 503, 504])
-
 s.mount("https://", HTTPAdapter(max_retries=retries))
 
+load_dotenv()
 data_dir = os.getenv("data_dir")
-member_csv = os.path.join(data_dir, "open_ai_members.csv")
+if data_dir is None:
+    data_dir = os.path.curdir
+
+date_list = os.getenv("date_range")
+if date_list is None:
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    date_list = [yesterday.strftime("%Y-%m-%d")]
+
+member_csv = os.path.join(data_dir, "openai_members.csv")
+usage_csv = os.path.join(data_dir, "openai_usage.csv")
 
 openai_org_id = os.getenv("openai_org_id")
 openai_api_key = os.getenv("openai_api_key")
 
-# # # this is for populating member list # # #
 
-header1 = {"Authorization": "Bearer {}".format(openai_api_key)}
-users = s.get("https://api.openai.com/v1/organizations/{}/users".format(openai_org_id),
-              headers=header1)
-user_data = json.loads(users.text)
+def get_members():
+    _header1 = {"Authorization": "Bearer {}".format(openai_api_key)}
+    _users = s.get("https://api.openai.com/v1/organizations/{}/users".format(openai_org_id),
+                  headers=_header1)
+    _user_data = json.loads(_users.text)
 
-member_list = [["userName", "userId", "userRole"]]
+    _member_list = [["userName", "userId", "userRole"]]
 
-for member in user_data["members"]["data"]:
-    user_name = member["user"]["name"]
-    user_id = member["user"]["id"]
-    user_role = member["role"]
-    member_list.append([user_name, user_id, user_role])
+    for _member in _user_data["members"]["data"]:
+        _user_name = _member["user"]["name"]
+        _user_id = _member["user"]["id"]
+        _user_role = _member["role"]
+        _member_list.append([_user_name, _user_id, _user_role])
 
-with open(member_csv, "w") as csv_file:
-    csvWriter = csv.writer(csv_file)
-    csvWriter.writerows(member_list)
+    print("Got {} members from OpenAI org".format(len(_member_list)-1))
+    return _member_list
 
 
-# # # this runs and logs the usage queries # # #
-date_list = os.getenv("date_range")
-if date_list is None:
-    now = datetime.datetime.now() - datetime.timedelta(days=1)
-    date_list = [now.strftime("%Y-%m-%d")]
+def write_members(_member_list):
+    print("Writing openai_members.csv")
+    with open(member_csv, "w") as _csv_file:
+        _csv_writer = csv.writer(_csv_file)
+        _csv_writer.writerows(_member_list)
 
-user_list = []
-with open(member_csv, "r") as csv_file:
-    csvReader = csv.reader(csv_file)
-    next(csvReader)  # skip header row
-    for row in csvReader:
-        user_list.append({"username": row[0], "id": row[1]})
 
-header2 = {"Authorization": "Bearer {}".format(openai_api_key),
-           "openai-organization": "{}".format(openai_org_id)}
+def get_and_write_usage(_members=None, from_file=False):
+    if _members is None:
+        _members = []
+    _user_list = []
+    if from_file:
+        with open(member_csv, "r") as csv_file:
+            _csv_reader = csv.reader(csv_file)
+            next(_csv_reader)  # skip header row
+            for row in _csv_reader:
+                _user_list.append({"username": row[0], "id": row[1]})
+    else:
+        for entry in _members[1:]:
+            _user_list.append({"username": entry[0], "id": entry[1]})
 
-usage_csv = os.path.join(data_dir, "open_ai_usage.csv")
+    _header2 = {"Authorization": "Bearer {}".format(openai_api_key),
+                "openai-organization": "{}".format(openai_org_id)}
 
-for user in user_list:
-    for day in date_list:
-        print(day, user["id"])
-        usage = s.get("https://api.openai.com/v1/usage?date={}&user_public_id={}".format(day, user["id"]),
-                      headers=header2)
-        usage_data = json.loads(usage.text)["data"]
-        if not usage_data:
-            print("no usage")
-        write_header = not os.path.exists(usage_csv)
-        with open(usage_csv, "a") as usage_file:
-            csvWriter2 = csv.writer(usage_file)
-            if write_header:
-                csvWriter2.writerow(["date",
-                                     "username",
-                                     "requests",
-                                     "model",
-                                     "context_tokens",
-                                     "generated_tokens"])
-            for batch in usage_data:
-                outData = [day,
-                           user["username"],
-                           batch["n_requests"],
-                           batch["snapshot_id"],
-                           batch["n_context_tokens_total"],
-                           batch["n_generated_tokens_total"]]
-                csvWriter2.writerow(outData)
+    for _user in _user_list:
+        for day in date_list:
+            print("Getting usage for {} on {}".format(_user["id"], day))
+            _usage = s.get("https://api.openai.com/v1/usage?date={}&user_public_id={}".format(day, _user["id"]),
+                           headers=_header2)
+            _usage_data = json.loads(_usage.text)["data"]
+            if _usage_data:
+                write_header = not os.path.exists(usage_csv)
+                with open(usage_csv, "a") as _usage_file:
+                    _csv_writer2 = csv.writer(_usage_file)
+                    if write_header:
+                        _csv_writer2.writerow(["date",
+                                               "username",
+                                               "requests",
+                                               "model",
+                                               "context_tokens",
+                                               "generated_tokens"])
+                    for _batch in _usage_data:
+                        _out_data = [day,
+                                     _user["username"],
+                                     _batch["n_requests"],
+                                     _batch["snapshot_id"],
+                                     _batch["n_context_tokens_total"],
+                                     _batch["n_generated_tokens_total"]]
+                        _csv_writer2.writerow(_out_data)
+
+
+def main():
+    members = get_members()
+    write_members(members)
+    get_and_write_usage(members)
+    # # alternatively if you already queried members (and don't expect changes) # #
+    # get_and_write_usage(from_file=True)
+
+
+if __name__ == "__main__":
+    main()
